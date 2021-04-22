9e23d38bfdb19d376e149e86066084e570fc846e
adylbek <aaaadyl.erg@gmail.com>
2021-04-22 20:04:35 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import "./app.scss"
 
-function App() {
+export default function App() {
+  const [fields, setFields] = useState(null)
+  const [mode, setMode] = useState('easyMode')
+  const [blocks, setBlocks] = useState([])
+  const fieldSizeByMode = (fields?.[mode]?.field || 5) * 10
+  const [colors, setColors] = useState([])
+
+  useEffect(() => {
+    fetch('https://demo1030918.mockable.io/')
+      .then(res => res.json())
+      .then(data => setFields(data))
+      .catch(error => alert(error.message))
+  }, [])
+
+  useEffect(() => {
+    setBlocks([])
+    setColors([])
+    for (let i = 0; i < 25; i++) {
+      setBlocks(s => [...s, i])
+      setColors(s => ({...s, [i]: true}))
+    }
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="actions">
+        <select name='field-set' value={mode} onChange={e => setMode(e.target.value)}>
+          <option value="easyMode">Easy mode</option>
+          <option value="normalMode">Normal mode</option>
+          <option value="hardMode">Hard mode</option>
+        </select>
+        <button>START</button>
+      </div>
+      <div className="table">
+        {blocks.length && blocks.map((b, i) =>
+          <div key={`${i}-table-blocks`}
+               style={{
+                 width: fieldSizeByMode,
+                 height: fieldSizeByMode,
+                 backgroundColor: colors[i] ? "#005cff" : "#fff"
+               }}
+               onMouseEnter={() => setColors(s => ({...s, [i]: !s[i]}))}
+               className="blocks"/>
+        )}
+      </div>
     </div>
   );
 }
-
-export default App;
